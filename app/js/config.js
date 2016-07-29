38ae005c081b4af56b04fca139e47414366d00e0
Edwin Chen <mredwinchenv@gmail.com>
2016-07-29 15:52:17 +0800

    Update

---
+++ app/js/config.js
@@ -0,0 +1,98 @@
+var gameObj = {
+	debug: true,
+	gameTime: 110,
+	playerState: {
+		name: "13422365678",
+		xingbi: 5000
+	},
+	autoPlayer: [
+		{
+			id: 'player1',
+			name: 'Edwin',
+			xingbi: 4000,
+        },
+		{
+			id: 'player2',
+			name: 'Jose',
+			xingbi: 4340,
+        },
+		{
+			id: 'player3',
+			name: 'Edward',
+			xingbi: 340,
+        },
+		{
+			id: 'player4',
+			name: 'Wenut',
+			xingbi: 1240,
+        },
+		{
+			id: 'player5',
+			name: 'Ken',
+			xingbi: 40,
+        },
+		{
+			id: 'player6',
+			name: 'Pael',
+			xingbi: 1,
+        },
+    ],
+	history: [
+		{
+			gameTime: 109,
+			num1: 'dice1',
+			num2: 'dice4',
+			txt: '顺子'
+		},
+		{
+			gameTime: 108,
+			num1: 'dice3',
+			num2: 'dice6',
+			txt: '23423'
+		},
+		{
+			gameTime: 107,
+			num1: 'dice2',
+			num2: 'dice5',
+			txt: '当时发生的'
+		}
+	],
+	state: 0
+};
+var sysObj = {
+	getXingbi: 'http://www.baidu.com',
+	nav: [
+		{
+			id: 'back',
+			link: 'javascript:;',
+			class: 'nav-1'
+		}, {
+			id: 'rule',
+			link: 'javascript:;',
+			class: 'nav-2'
+		}
+	],
+	btn: [
+		{
+			id: '01',
+			value: '100'
+		},
+		{
+			id: '02',
+			value: '200'
+		},
+		{
+			id: '03',
+			value: '500'
+		},
+		{
+			id: '04',
+			value: '1000'
+		},
+		{
+			id: '05',
+			value: '5000'
+		}
+	],
+	rule: '一般来说，在设计类的时候，我们希望能减少重复性的代码，并且尽量弱化对象间的耦合。使用继承符合前一个设计原则的需要。借助这种机制，你可以在现有类的基础上进行设计并充分利用它们已经具备的各种方法，而对设计进行修改也更为轻松。假设你需要让几个类都拥有一个按特定方式输出类结构的toString()方法，当然可以用复制加粘贴的办法把定义toString()方法的代码添加到每一个类中，但这样做的话，每当需要改变这个方法的工作方式时，你将不得不在每一个类中重复同样的修改。反之，如果你提供了一个ToStringProvider类，然后让那些类继承这个类，那么toString这个方法只需在一个地方声明即可。让一个类继承另一个类可能会导致二者产生强耦合，也即一个类的依赖于另一个类的内部实现。我们将讨论一些有助于避免这种问题的技术，其中包括用掺元类为其他类提供方法这种技术。'
+};
---
+++ app/js/main.js
@@ -0,0 +1,229 @@
+//判断手机横竖屏
+function anyWay() {
+	var anywayTips = $(".anyway");
+
+	function horizontal() {
+		if (window.orientation == 180 || window.orientation == 0) {
+			anywayTips.show();
+		}
+	}
+
+	function vertical() {
+		if (window.orientation == 90 || window.orientation == -90) {
+			anywayTips.hide();
+		}
+	}
+	horizontal();
+	vertical();
+	window.addEventListener('orientationchange', function (event) {
+		horizontal();
+		vertical();
+	});
+	//判断设备宽度
+	var width = window.innerWidth || document.documentElement.clientWidth;
+	if (width < 768) {
+		console.log("您在使用手机浏览");
+	} else if (width >= 768) {
+		console.log("您在使用平板浏览");
+		anywayTips.hide();
+	} else if (width >= 992) {
+		console.log("您在使用电脑浏览");
+		anywayTips.hide();
+	} else if (width >= 1200) {
+		console.log("您在使用大屏电脑浏览");
+		anywayTips.hide();
+	} else {
+		console.log("找不到你的设备尺寸");
+	}
+}
+anyWay();
+
+//手机加载loading
+function loading() {
+	window.onload = function () {
+		try {
+			document.body.removeChild(document.getElementById('loading'));
+		} catch (e) {}
+		try {
+			initEvent(); //固定加载事件方法
+		} catch (e) {}
+		try {
+			doAction(); //每个页面都必须存在，用来执行该页面的方法
+		} catch (e) {}
+	}
+}
+loading();
+
+
+//Header动画交互
+function heaAn(OClass, Num, Speed) {
+	$(OClass).animate({
+		top: Num
+	}, Speed);
+}
+
+//Footer动画交互Show
+function fooAn(OClass, Num, Speed) {
+	$(OClass).animate({
+		bottom: Num
+	}, Speed);
+}
+
+
+//玩家动画交互Show
+function pncAn(Num) {
+	var pnc = 6;
+	for (var a = 0; a < pnc; a++) {
+		var c = a + 1;
+		if (c > 3) {
+			$(".player:nth-child(" + c + ")").animate({
+				right: Num
+			}, 300 * c);
+		} else {
+			$(".player:nth-child(" + c + ")").animate({
+				left: Num
+			}, 600 * c);
+		}
+	}
+}
+
+//赌桌动画交互
+function conAn(ObjName, OMargin, OWidth, OHeight, Speed) {
+	$(ObjName).animate({
+		margin: OMargin,
+		width: OWidth,
+		height: OHeight
+	}, Speed);
+}
+
+//隐藏于显示交互
+function clickShow(a, b) {
+	$(a).click(function () {
+		$(b).show();
+		$(b).click(function () {
+			$(this).hide();
+		})
+	});
+}
+
+/*白色背景模态框插件*/
+(function ($) {
+	$.fn.modal = function (options) {
+		var modalparameter = {
+			showbtn: "",
+			modaltitle: "",
+			modalcontent: ""
+		};
+		var opts = $.extend(modalparameter, options);
+		$(this).append('<div class="modal">' +
+			'<div class="modal-content">' +
+			'<h3 class="title">' + modalparameter.modaltitle + '</h3>' +
+			'<p class="txt">' + modalparameter.modalcontent + '</p>' +
+			'</div>' +
+			'</div>');
+		modalparameter.showbtn.click(function () {
+			$('.modal').css('display', 'block');
+		});
+		$('.modal').click(function () {
+			$(this).hide();
+		});
+	}
+})(jQuery);
+
+
+//倒计时
+function secondCounter1(defSec, func, dispObj) {
+
+	document.getElementById(dispObj).innerHTML = defSec--;
+	if (defSec < 0) {
+		eval(func);
+		window.setTimeout("secondCounter1(" + document.getElementById(dispObj).getAttribute("defSec") + ",\"" + func + "\",\"" + dispObj + "\")", 1000);
+	} else
+		window.setTimeout("secondCounter1(" + defSec + ",\"" + func + "\",\"" + dispObj + "\")", 1000);
+}
+
+//游戏初始化
+function initializeGame() {
+	//$("body").append('<div class="countdown"><p>' + gameObj.playerState.name + ' 欢迎回来，当前<span id="nowGameTime"></span>期</p><span class="cdt-cell">游戏即将开始，请稍后(<span class="cdt-num" id="cdt" defSec="10"></span>)</span></div>')
+	$("#name").html(gameObj.playerState.name);
+	$("#xingbi").html(gameObj.playerState.xingbi);
+	$("#nowGameTime").html(gameObj.gameTime);
+	$("#link").attr("href", sysObj.getXingbi)
+	var Objlength = gameObj.autoPlayer.length;
+	for (var i = 0; i < Objlength; i++) {
+		$("#autoPlayer").append('<div class="player"><span class="player-num"><span>' + gameObj.autoPlayer[i].xingbi + '</span>万</span></div>');
+	}
+	$("#lastTime").append('<span class="date">' + gameObj.history[0].gameTime + '期</span><span class="numbroup"><span class="num ' + gameObj.history[0].num1 + '"></span><span class="num ' + gameObj.history[0].num2 + '"></span></span><span class="txt">' + gameObj.history[0].txt + '</span>');
+	var hilength = gameObj.history.length;
+	for (var i = 0; i < hilength; i++) {
+		$("#historyList").append('<li><span class="date">' + gameObj.history[i].gameTime + '期</span><span class="numbroup"><span class="num ' + gameObj.history[i].num1 + '"></span><span class="num ' + gameObj.history[i].num2 + '"></span></span><span class="txt">' + gameObj.history[i].txt + '</span></li>')
+	}
+	var liLen = 20;
+	for (var l = 0; l < liLen; l++) {
+		var j = l + 1;
+		$("#gameContent").append('<li>' + j + '</li>')
+	}
+	var Navlength = sysObj.nav.length;
+	for (var n = 0; n < Navlength; n++) {
+		$("#navList").append('<li><a href="' + sysObj.nav[n].link + '" class="' + sysObj.nav[n].class + '" id="' + sysObj.nav[n].id + '"></a></li>')
+	}
+	var Btnlength = sysObj.btn.length;
+	for (var n = 0; n < Btnlength; n++) {
+		$("#btnGroup").append('<button class="gre-btn" value="' + sysObj.btn[n].value + '">' + sysObj.btn[n].value + '</button>');
+	}
+	$("#ruleModal").modal({
+		showbtn: $('#rule'),
+		modaltitle: '游戏规则',
+		modalcontent: sysObj.rule,
+	});
+}
+initializeGame();
+
+//游戏交互
+function interactionGame() {
+	heaAn(".header", "0", 500);
+	fooAn(".footer", "0", 900);
+	pncAn("0");
+	$("#fullpage").click(function () {
+		heaAn(".header", "-120", 500);
+		pncAn("-120");
+		conAn(".content", "4.9% auto", "92%", "70%", 600);
+		$(".game").append('<span class="header-btn"><i></i></span>');
+		$(".header-btn").bind("click", function () {
+			heaAn(".header", "0", 500);
+			pncAn("0");
+			conAn(".content", "9.6% auto", "80%", "65%", 600);
+			$(this).remove();
+		})
+	});
+	clickShow(".nav-btn", ".nav-list");
+	clickShow(".header-content", ".history");
+}
+interactionGame();
+
+//游戏进行Step
+
+//secondCounter1(10, "step1()", "cdt");
+
+function step1() {
+	$(".countdown").hide();
+	step2();
+}
+
+function step2() {
+	$(".bagin").show();
+	secondCounter1(10, "step3()", "bcdt");
+}
+
+function step3() {
+	$(".bagin").hide();
+	step4();
+}
+
+function step4() {
+	$(".content").find("li").click(function () {
+		var liVal = $(this).text();
+		console.log(liVal);
+	});
+}
+step4();
